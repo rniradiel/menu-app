4ff2060533039be8ef4b94ad420f6370abc178ef
rniradiel <rniradiel@gmail.com>
2024-06-06 18:04:08 +0800

    Clean unused import

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import Header from './components/header/header.js';
 import Product from './product/page.js';
 import ProductItem from './product/product-item/page.js';
-import Modal from './components/modal/page.js';
 import Cart from './cart/cart.js';
 import Notification from './components/notification/notification.js';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
